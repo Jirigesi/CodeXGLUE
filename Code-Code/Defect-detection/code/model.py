191f808e80de1d10e1dfb8da0155529ef933e1ad
jirigesi <fjiriges@uci.edu>
2022-11-28 15:34:36 -0800

    fix attribution analysis bug for codebert

--- Code-Code/Defect-detection/code/model.py
+++ Code-Code/Defect-detection/code/model.py
@@ -4,18 +4,23 @@ import torch
 import torch.nn as nn
 import torch
 
-    
 class Model(nn.Module):   
-    def __init__(self, encoder,config, tokenizer, args):
+    def __init__(self, encoder, config, tokenizer, args):
         super(Model, self).__init__()
         self.encoder = encoder
         self.config=config
         self.tokenizer=tokenizer
         self.args=args
         
-    def forward(self, input_ids=None,labels=None): 
-        outputs=self.encoder(input_ids,attention_mask=input_ids.ne(1))[0]
-        logits=outputs
+    def forward(self, 
+                input_ids=None,
+                attention_mask=None,
+                labels=None,
+                inputs_embeds=None): 
+        outputs=self.encoder(input_ids, 
+                             attention_mask=attention_mask,
+                             inputs_embeds=inputs_embeds)
+        logits=outputs[0]
         prob=torch.sigmoid(logits)
         
         if labels is not None:
@@ -23,7 +28,7 @@ class Model(nn.Module):
             # self write loss function nn.BCELoss()
             loss=torch.log(prob[:,0]+1e-10)*labels+torch.log((1-prob)[:,0]+1e-10)*(1-labels)
             loss=-loss.mean()
-            return loss,prob
+            return loss,prob, outputs.attentions
         else:
             return prob
       
